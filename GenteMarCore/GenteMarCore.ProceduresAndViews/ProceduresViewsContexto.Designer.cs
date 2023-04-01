552957e38f39fec9d5ea9e6f27439c0dc38b8201
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2023-03-31 19:00:43 -0500

    Actualización a net framework 4.8

--- GenteMarCore/GenteMarCore.ProceduresAndViews/ProceduresViewsContexto.Designer.cs
+++ GenteMarCore/GenteMarCore.ProceduresAndViews/ProceduresViewsContexto.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Carpetas Windows\Descargas\GenteDeMar-797af6dbd2276c4c1c65e5e54463187ea091e340\GenteMarCore\GenteMarCore.ProceduresAndViews\ProceduresViewsContexto.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\DIMAR\GenteMarApi\GenteMarCore\GenteMarCore.ProceduresAndViews\ProceduresViewsContexto.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
